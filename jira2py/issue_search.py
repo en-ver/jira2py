0db612fc6b9abbfef10fde3121f52902cb75874a
en-ver <>
2025-04-23 07:37:56 +0300

    fix

--- jira2py/issue_search.py
+++ jira2py/issue_search.py
@@ -16,7 +16,7 @@ class IssueSearch(JiraBase):
         fields_by_keys: bool = False,
         fail_fast: bool = False,
         reconcile_issues: list[int] | None = [],
-    ) -> list[dict]:
+    ) -> dict:
         """Searches for issues using JQL.
         https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-search/#api-rest-api-3-search-jql-post
 
