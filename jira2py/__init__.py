e71907b0d4689c0bf3970cbccb966b03f153938a
en-ver <enver.ablyaev@gmail.com>
2025-08-06 20:08:48 +0300

    chore: remove JiraBase and related modules

--- jira2py/__init__.py
+++
@@ -1,6 +0,0 @@
-from .issue_fields import IssueFields
-from .issue_search import IssueSearch
-from .issues import Issues
-from .issue_comments import IssueComments
-
-__all__ = ["IssueFields", "IssueSearch", "Issues", "IssueComments"]
